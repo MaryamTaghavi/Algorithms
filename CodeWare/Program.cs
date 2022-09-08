5844949bded2e200ee2e8998a661874e9e65a2f1
MaryamTaghavi <m.taghavi.ce@gmail.com>
2022-09-08 18:46:29 +0430

    Add project files.

---
+++ CodeWare/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CodeWare_SumOfDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DigitalRoot(129);
+        }
+        public static int DigitalRoot(long n)
+        {
+            string str = n + "";
+            char[] digits = str.ToCharArray();
+
+            int sum = 0;
+            for (int i = 0; i < digits.Length; i++)
+                sum += (int)Char.GetNumericValue(digits[i]);
+
+            if ((sum + "").ToCharArray().Length == 1)
+                return sum;
+            
+            return DigitalRoot(sum);
+        }
+    }
+}
---
+++ SplitStrings/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace CodeWare_SplitStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "abc";
+            string[] solu = Solution(str);
+        }
+        public static string[] Solution(string str)
+        {
+            List<string> list = new List<string>();
+            for (int i = 0; i < str.Length; i = i + 2)
+            {
+                if (i == str.Length - 1 || i == str.Length - 2)
+                {
+                    if (str.Length % 2 != 0)
+                    {
+                        list.Add(str.Substring(i) + "_");
+                        continue;
+                    }
+                    else
+                    {
+                        list.Add(str.Substring(i));
+                        continue;
+                    }
+                }
+                list.Add(str.Substring(i, 2));
+            }
+            return list.ToArray();
+        }
+
+    }
+}
