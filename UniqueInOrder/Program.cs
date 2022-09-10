456831e4a4112236794e1e937f6f2a421853e5d1
MaryamTaghavi <m.taghavi.ce@gmail.com>
2022-09-10 14:24:45 +0430

    YourOrder_Please

---
+++ UniqueInOrder/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UniqueInOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UniqueInOrder("AABBCDDBB");
+        }
+        public static IEnumerable<T> UniqueInOrder<T>(IEnumerable<T> iterable)
+        {
+            char[] chars = iterable.ToString().ToCharArray();
+            List<string> finalString = new List<string>();
+
+            for(int i = 0; i < chars.Length; i++)
+            {
+                if(!CheckEqualNextCharacters(chars[i] , chars[i+1]) && !finalString.Contains(chars[i].ToString()))
+                    finalString.Add(chars[i].ToString());
+            }
+            return finalString.GetEnumerator();
+        }
+        public static bool CheckEqualNextCharacters(char ch , char nextCh)
+        {
+            if (ch.Equals(nextCh))
+                return true;
+
+            return false;
+        }
+    }
+}
---
+++ YourOrder-Please/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace YourOrder_Please
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result =Order("is2 Thi1s T4est 3a");
+        }
+        public static string Order(string words)
+        {
+            //Solution1
+            string result = "";
+            if (String.IsNullOrEmpty(words))
+                return result;
+            string[] str;
+            str = words.Split(' ');
+            for (int i = 0; i < str.Length; i++)
+            {
+                for (int j = 0; j < str.Length; j++)
+                    if (str[j].Contains((i + 1).ToString()))
+                    {
+                        if (i == str.Length - 1)
+                        {
+                            result += str[j].ToString();
+                            break;
+                        }
+
+                        result += str[j].ToString() + " ";
+                        break;
+                    }
+            }
+
+            return result;
+
+            //Solution2
+            return string.Join(" ", words.Split(' ').OrderBy(s => s.ToList().Find(c => char.IsDigit(c))));
+        }
+    }
+}
