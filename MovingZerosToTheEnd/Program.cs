7ef6edbd2d34be3c54191193bcd6e0bafe5d1ef3
Maryam Taghavi <m.taghavi.ce@gmail.com>
2023-03-09 22:26:39 +0330

    MovingZerosToTheEnd

---
+++ MovingZerosToTheEnd/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace MovingZerosToTheEnd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int[] solu = MoveZeroes(new int[] { 1,0, 2, 1,0, 1, 3,0, 1});
+            Console.WriteLine("Helllllo");
+        }
+        static int[] MoveZeroes(int[] arr)
+        {
+            return arr.OrderByDescending(x => x == 0).ToArray();
+
+            // return arr.Where(e => e != 0).Concat(arr.Where(e => e == 0)).ToArray();
+        }
+
+    }
+}
