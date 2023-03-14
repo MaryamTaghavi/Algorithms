d00ae8a98da6af74daceaae5c10aef87b0d5cb1b
Maryam Taghavi <m.taghavi.ce@gmail.com>
2023-03-14 16:58:51 +0330

    AddingBigNumbers

---
+++ AddingBigNumbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Numerics;
+
+namespace AddingBigNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string solu = Add("999999999", "1");
+        }
+        public static string Add(string a, string b)
+        {
+            return (BigInteger.Parse(a) + BigInteger.Parse(b)).ToString();
+        }
+    }
+}
