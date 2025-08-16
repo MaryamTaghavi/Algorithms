63b2f65397ef678dd6cf382cfc9508a8409574d0
MaryamTaghavi <m.taghavi@megagroup.biz>
2025-08-16 15:23:34 +0330

    addAddressToArray

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -1,4 +1,6 @@
-﻿/* Given a fixed-length integer array arr, duplicate each occurrence of zero, shifting the remaining elements to the right.
+﻿// LeetCode : https://leetcode.com/explore/learn/card/fun-with-arrays/525/inserting-items-into-an-array/3245/
+
+/* Given a fixed-length integer array arr, duplicate each occurrence of zero, shifting the remaining elements to the right.
  Note that elements beyond the length of the original array are not written. Do the above modifications to the input array in place and do not return anything. */
 
 /* Input: arr = [1,0,2,3,0,4,5,0]
