a015128bda83ebd9e959f4e152485fc2894b343b
MaryamTaghavi <m.taghavi@megagroup.biz>
2025-08-19 14:43:29 +0330

    changeValidMountainArray

--- Arrays/ValidMountainArray/Program.cs
+++ Arrays/ValidMountainArray/Program.cs
@@ -11,36 +11,39 @@ arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
 
  */
 
-var result = ValidMountainArray([2, 1, 2, 3, 5, 7, 9, 10, 12, 14, 15, 16, 18, 14, 13]);
+var result = ValidMountainArray([9, 8, 7, 6, 5, 4, 3, 2, 1, 0]);
 Console.WriteLine(result);
 Console.ReadLine();
 
 bool ValidMountainArray(int[] arr)
 {
     var counter = false;
+    var length = arr.Length - 1;
+    var i = 0;
 
     if (arr.Length < 3)
     {
         return counter;
     }
 
-    for (int i = 0; i < arr.Length - 1; i++)
+    while (i < length && arr[i + 1] > arr[i])
     {
-        if (i > 0 && arr[i + 1] < arr[i] && arr[i - 1] < arr[i])
+        i++;
+    }
+
+    while (i < length && i > 0)
+    {
+        if (arr[i + 1] >= arr[i])
+        {
+            counter = false;
+            break;
+        }
+        else
         {
             counter = true;
-            continue;
         }
 
-        if(!counter && arr[i + 1] < arr[i])
-        {
-            return false;
-        }
-
-        if (counter && arr[i] <= arr[i + 1] || arr[i] == arr[i + 1])
-        {
-            return false;
-        }
+        i++;
     }
 
     return counter;
