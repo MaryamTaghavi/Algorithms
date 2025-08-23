20c9b55080a7aa4c0ff54d99b6d453403f2f0f6a
MaryamTaghavi <m.taghavi@megagroup.biz>
2025-08-23 12:57:21 +0330

    createAbstractDataType

---
+++ MainMeanings/AbstractDataTypes/Program.cs
@@ -0,0 +1,17 @@
+﻿/* An Abstract Data Type (ADT) is a theoretical concept in computer science that defines
+ a data structure by its behavior rather than its implementation. In simpler terms */
+
+/*
+It specifies what operations can be performed on the data and what those operations do, 
+without specifying how the data is stored or how the operations are implemented. */
+
+/*
+
+Common Examples of ADTs:
+
+Stack – Supports operations like push, pop, peek.
+Queue – Supports enqueue, dequeue, front.
+List – Supports operations like insert, delete, get.
+Map/Dictionary – Supports put, get, remove.
+
+*/
