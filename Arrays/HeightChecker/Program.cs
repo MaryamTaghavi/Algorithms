b734eabe9f6eb001987dc1c885a7dbeed32f20bb
MaryamTaghavi <m.taghavi@megagroup.biz>
2025-08-24 16:13:24 +0330

    addHeightChecker

---
+++ Arrays/HeightChecker/Program.cs
@@ -0,0 +1,19 @@
+﻿// LeetCode : https://leetcode.com/explore/learn/card/fun-with-arrays/523/conclusion/3228/
+
+Console.WriteLine(HeightChecker([5, 1, 4, 2, 1,0, 3]));
+Console.ReadLine();
+
+static int HeightChecker(int[] heights)
+{
+    int[] expected = heights.OrderBy(h => h).ToArray();
+
+    int mismatchCount = 0;
+
+    for (int i = 0; i < heights.Length; i++)
+    {
+        if (heights[i] != expected[i])
+            mismatchCount++;
+    }
+
+    return mismatchCount;
+}
