f7c51ac9c4f31722054a6ec445e14b8ca086632c
MaryamTaghavi <m.taghavi@megagroup.biz>
2025-08-24 21:12:44 +0330

    addBubbleSortAlgorithm

--- SortingArray/BubbleSort/Program.cs
+++ SortingArray/BubbleSort/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Leetcode : https://leetcode.com/problems/height-checker/editorial/
+// Time Complexity: O(n^2)
+
+
+var result = SortArrays([10, 2, 4, 5, 3, 7, 9]);
+Console.ReadLine();
+
+int[] SortArrays(int[] arr)
+{
+    var length = arr.Length - 1 ;
+    for (int i = 0; i < length; i++)
+    {
+        for(int j = 0; j < length - i; j++)
+        {
+            if( arr[j] > arr[j+1])
+            {
+                var temp = arr[j + 1];
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    return arr;
+}
