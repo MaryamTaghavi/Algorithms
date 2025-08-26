039a7dbf41a350e0a9847525d33923637dd8b203
MaryamTaghavi <m.taghavi@megagroup.biz>
2025-08-26 16:48:05 +0330

    createDailyTemperature

---
+++ Stack/DailyTemperatures/Program.cs
@@ -0,0 +1,44 @@
+﻿// Leetcode : https://leetcode.com/problems/daily-temperatures/description/
+
+/*
+Given an array of integers temperatures represents the daily temperatures, 
+return an array answer such that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature. 
+If there is no future day for which this is possible, keep answer[i] == 0 instead.
+ */
+
+// Stack نگه می‌داره اندیس روزهایی که هنوز جوابشون پیدا نشده.
+var result = DailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]);
+Console.ReadLine();
+
+static int[] DailyTemperatures(int[] temperatures)
+{
+    Stack<int> stack = new Stack<int>();
+    int[] result = new int[temperatures.Length];
+
+    if(temperatures.Count() > 0)
+    {
+        stack.Push(temperatures[0]);
+    }
+    for(int i = 0; i < temperatures.Length; i++)
+    {
+        if (temperatures[i+1] > temperatures[i])
+        {
+            if (stack.Count > 0)
+            {
+                var pop = stack.Pop();
+                result[i] = i - pop;
+            }
+            else
+            {
+                result[i] = i;
+            }
+        }
+
+        else
+        {
+            stack.Push(temperatures[i]);
+        }
+    }
+
+    return result;
+}
