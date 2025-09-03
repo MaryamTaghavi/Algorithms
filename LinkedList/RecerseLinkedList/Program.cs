b82708c63d1142ec33e540f0c0fa83b44eec97b9
MaryamTaghavi <m.taghavi@megagroup.biz>
2025-09-03 14:24:08 +0330

    addReverseBetween

--- LinkedList/RecerseLinkedList/Program.cs
+++ LinkedList/RecerseLinkedList/Program.cs
@@ -1,24 +1,39 @@
 ﻿
 
-ReverseList(new ListNode()
-{
-    next = new ListNode(),
-    val = 2
-});
+ListNode head = new ListNode(1);
+head.next = new ListNode(2);
+head.next.next = new ListNode(3);
+head.next.next.next = new ListNode(4);
+
+ReverseList(head);
 
 ListNode ReverseList(ListNode head)
 {
-    
-    return null;
+    ListNode current = head;
+    ListNode pre = null;
+
+    if (current != null)
+    {
+        while (current != null)
+        {
+            ListNode next = current.next;
+            current.next = pre;
+            pre = current;
+            current = next;
+        }
+    }
+
+    return pre;
 }
 
 public class ListNode
 {
-     public int val;
-     public ListNode next;
-     public ListNode(int val = 0, ListNode next = null)
-     {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
         this.val = val;
         this.next = next;
-     }
- }
+    }
+}
---
+++ LinkedList/ReverseLinkedList2/Program.cs
@@ -0,0 +1,52 @@
+﻿//LeetCode : https://leetcode.com/problems/reverse-linked-list-ii/description/?utm_source=chatgpt.com
+
+ListNode head = new ListNode(1);
+head.next = new ListNode(3);
+head.next.next = new ListNode(5);
+head.next.next.next = new ListNode(2);
+head.next.next.next.next = new ListNode(10);
+
+ReverseBetween(head, 2, 4);
+
+ListNode ReverseBetween(ListNode head, int left, int right)
+{
+    if (head == null || left == right) 
+        return head ;
+
+    ListNode dummy = new ListNode(0, head) ;
+    ListNode prev = dummy ;
+
+    for (int i = 1; i < left; i++)
+    {
+        prev = prev.next;
+    }
+
+    ListNode curr = prev.next ;
+    ListNode next = null ;
+    ListNode prevSub = null ;
+
+    for (int i = 0; i <= right - left; i++)
+    {
+        next = curr.next ;
+        curr.next = prevSub ;
+        prevSub = curr;
+        curr = next;
+    }
+
+    prev.next.next = curr ;
+    prev.next = prevSub ;
+
+    return dummy.next;
+}
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
