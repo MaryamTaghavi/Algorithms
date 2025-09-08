4670023e555ad5dcd1d18fd38087388d11ab3e39
MaryamTaghavi <m.taghavi.ce@gmail.com>
2025-09-08 17:07:30 +0330

    completeStackWithQueue

--- Queue/ImplementStackUsingQueues/Program.cs
+++ Queue/ImplementStackUsingQueues/Program.cs
@@ -1,3 +1,55 @@
 ﻿// LeetCode : https://leetcode.com/problems/implement-stack-using-queues/description/
 
-// کامل نیست
+
+MyStack myStack = new MyStack();
+myStack.Push(1);
+myStack.Push(2);
+myStack.Push(3);
+
+myStack.Top(); // return 3
+myStack.Pop(); // return 3
+myStack.Empty(); // return False
+
+public class MyStack
+{
+    Queue<int> queue1 = new Queue<int>();
+    Queue<int> queue2 = new Queue<int>();
+
+    public MyStack()
+    {
+    }
+
+    public void Push(int x)
+    {
+        while (queue1.Count > 0)
+        {
+             queue2.Enqueue(queue1.Dequeue());
+        }
+
+        queue1.Enqueue(x);
+
+        while (queue2.Count > 0)
+        {
+            queue1.Enqueue(queue2.Dequeue());
+        }
+    }
+
+    public int Pop()
+    {
+        return queue1.Dequeue();
+    }
+
+    public int Top()
+    {
+        var top = queue1.Dequeue();
+
+        Push(top);
+
+        return top ;
+    }
+
+    public bool Empty()
+    {
+        return queue1.Count() == 0;
+    }
+}
