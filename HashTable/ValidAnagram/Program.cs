533d7337f8f9faa34a0543b86d5e620c9bcd0d92
MaryamTaghavi <m.taghavi.ce@gmail.com>
2025-09-09 22:33:12 +0330

    completeHashTableAlgorithm

---
+++ HashTable/ValidAnagram/Program.cs
@@ -0,0 +1,50 @@
+﻿// LeetCode : https://leetcode.com/problems/valid-anagram/description/
+
+Console.WriteLine(IsAnagram("rat", "car"));
+Console.ReadLine();
+
+bool IsAnagram(string s, string t)
+{
+    char[] str = s.ToCharArray();
+    char[] str1 = t.ToCharArray();
+
+    if (str.Length != str1.Length)
+    {
+        return false;
+    }
+
+    Dictionary<char,int> dic = new Dictionary<char,int>();
+    Dictionary<char,int> dic1 = new Dictionary<char,int>();
+
+    for (int i = 0 ; i < str.Length ; i++)
+    {
+        if (dic.ContainsKey(str[i]))
+        {
+            dic[str[i]] += 1;
+        }
+        else
+        {
+            dic.Add(str[i], 1);
+        }
+    }
+
+    for (int i = 0; i < str1.Length; i++)
+    {
+        if (dic1.ContainsKey(str1[i]))
+        {
+            dic1[str1[i]] += 1;
+        }
+        else
+        {
+            dic1.Add(str1[i], 1);
+        }
+    }
+
+    for (int i = 0; i < str.Length; i++)
+    {
+        if (!dic1.ContainsKey(str[i]) || dic[str[i]] - dic1[str[i]] != 0)
+            return false;
+    }
+
+    return true;
+}
