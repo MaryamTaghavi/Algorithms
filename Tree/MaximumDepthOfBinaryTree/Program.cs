ad0f998e44e61eea23bd63db7d0b138c1eb66924
MaryamTaghavi <m.taghavi.ce@gmail.com>
2025-09-12 13:09:05 +0330

    completeMaximumDepthOfBinaryTree

--- Tree/MaximumDepthOfBinaryTree/Program.cs
+++ Tree/MaximumDepthOfBinaryTree/Program.cs
@@ -1,1 +1,31 @@
 ﻿// LeetCode : https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+
+
+TreeNode root = new TreeNode();
+root.val = 3;
+root.left = new TreeNode() { val = 9, left = null, right = null }; ;
+root.right = new TreeNode() { val = 20, left = new TreeNode() { val = 15 }, right = new TreeNode() { val = 7 } };
+
+Console.WriteLine(MaxDepth(root));
+Console.ReadLine();
+
+// این الگ.ریتم به روش DFS (جستجو در عمق حل شده است)
+int MaxDepth(TreeNode root)
+{
+    if (root == null) return 0;
+    return Math.Max(MaxDepth(root.left), MaxDepth(root.right)) + 1;
+}
+
+// Definition for a binary tree node.
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
