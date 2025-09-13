43b85b1dc41575236b3d6754db7403c500e5d849
MaryamTaghavi <m.taghavi.ce@gmail.com>
2025-09-13 11:26:44 +0330

    createBinarySearchTreeProj

---
+++ Tree/BinarySearchTree/Program.cs
@@ -0,0 +1,20 @@
+﻿// https://leetcode.com/problems/search-in-a-binary-search-tree/description/
+
+TreeNode SearchBST(TreeNode root, int val)
+{
+
+}
+
+// Definition for a binary tree node.
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
