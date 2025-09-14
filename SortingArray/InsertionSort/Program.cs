f9b1ccceb867a6f49a610acbaf87c1c3b87f32bd
MaryamTaghavi <m.taghavi.ce@gmail.com>
2025-09-14 10:27:52 +0330

    createInsertionSortListWithLinkedList

--- SortingArray/InsertionSort/Program.cs
+++ SortingArray/InsertionSort/Program.cs
@@ -1,16 +1,7 @@
-﻿// LeetCode : https://leetcode.com/problems/insertion-sort-list/description/ => LinkedList
-// LeetCode : https://leetcode.com/problems/sort-an-array/description/ => Array
+﻿// LeetCode : https://leetcode.com/problems/sort-an-array/description/ => Array
 
 // In-Place حل میشود در بهترین حالت که آرایه مرتب است O(n)
 // و در بدترین حالت O(n2)
-
-
-//ListNode head = new ListNode(1);
-//head.next = new ListNode(3);
-//head.next.next = new ListNode(5);
-//head.next.next.next = new ListNode(2);
-//head.next.next.next.next = new ListNode(11);
-//head.next.next.next.next.next = new ListNode(10);
 
 //int[] array = new int[5] {1,5,3,2,9};
 //int[] array = new int[0];
@@ -37,16 +28,4 @@ int[] InsertionSortList(int[] head)
     }
 
     return head;
-}
-
-public class ListNode
-{
-    public int val;
-    public ListNode next;
-
-    public ListNode(int val = 0, ListNode next = null)
-    {
-        this.val = val;
-        this.next = next;
-    }
 }
---
+++ SortingArray/InsertionSortWithLinkedList/Program.cs
@@ -0,0 +1,26 @@
+﻿// LeetCode : https://leetcode.com/problems/insertion-sort-list/description/ => LinkedList
+
+ListNode head = new ListNode(1);
+head.next = new ListNode(3);
+head.next.next = new ListNode(5);
+head.next.next.next = new ListNode(2);
+head.next.next.next.next = new ListNode(11);
+head.next.next.next.next.next = new ListNode(10);
+
+
+ListNode InsertionSortList(ListNode head)
+{
+    return null;
+}
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
