1d44710d0859f69794bb43570d8ae4b161d84205
MaryamTaghavi <m.taghavi.ce@gmail.com>
2025-09-14 08:22:55 +0330

    FirstNonRepeatingCharacter

---
+++ Free/FirstNonRepeatingCharacter/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace FirstNonRepeatingCharacter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string solu = FirstNonRepeatingLetter("sTreSS");
+        }
+        public static string FirstNonRepeatingLetter(string s)
+        {
+            return s.GroupBy(char.ToLower)
+            .Where(gr => gr.Count() == 1)
+            .Select(x => x.First().ToString())
+            .First();
+
+            //return s.GroupBy(char.ToLower).First(r => r.Count() == 1).Key.ToString();
+
+        
+        }
+    }
+}
